065cb57f4a5d00b2d926e116df3067cebef64b67
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-02 12:45:04 -0700

    refactor RepeatCounter.cs CountRepeats method to pass first test.

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -7,7 +7,16 @@ namespace WordCounter
   {
     public int CountRepeats(string firstInput, string secondInput)
     {
-      return 0;
+      int matchCount = 0;
+      if (secondInput.Contains(firstInput))
+      {
+        matchCount += 1;
+      }
+      else
+      {
+        matchCount = 0;
+      }
+      return matchCount;
     }
   }
 }
