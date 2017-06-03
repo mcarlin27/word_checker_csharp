ce54bebb002b65d8e956d74901d70e546ded1dfc
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-02 17:02:54 -0700

    add results.cshtml file and Post Request to access it to HomeModule.cs; program does not run currently.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,18 @@ namespace WordCounter
       Get["/"] = _ => {
         return View["index.cshtml"];
       }; //homepage
+      Post["/"] = _ => {
+        RepeatCounter wordCheck = new RepeatCounter();
+        string firstInput = Request.Query["first-input"];
+        string secondInput = Request.Query["second-input"];
+        int output = wordCheck.CountRepeats(firstInput, secondInput);
+        string stringOutput = output.ToString();
+        Dictionary<string, string> model = new Dictionary<string, string>();
+        model.Add("first-input", firstInput);
+        model.Add("instances", stringOutput);
+        model.Add("second-input", secondInput);
+        return View["results.cshtml", model];
+      }; //displays results
     }
   }
 }
